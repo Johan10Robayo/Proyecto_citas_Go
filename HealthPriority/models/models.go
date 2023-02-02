47dcf8fe16634b6feb1d1d7410d4e0a60b03c48b
Johan_Robayo <johan.robayo@unillanos.edu.co>
2023-02-01 19:23:13 -0500

    actualización de las migraciones

--- HealthPriority/models/models.go
+++ HealthPriority/models/models.go
@@ -3,7 +3,7 @@ package models
 import "time"
 
 type Estado struct {
-	Id                 int64  `gorm:"primaryKey;autoIncrement:true"`
+	Id                 int64  `gorm:"primaryKey;autoIncrement:false"`
 	Comorbilidad       string `gorm:"type:varchar(50)"`
 	Nivel_comorbilidad int    `gorm:"type:tinyint"`
 	Embarazo           bool
@@ -29,7 +29,7 @@ type Login struct {
 }
 
 type Cita struct {
-	Id         int64     `gorm:"primaryKey;autoIncrement:true"`
+	Id         int64     `gorm:"primaryKey;autoIncrement:false"`
 	Fecha_hora time.Time `gorm:"type:timestamp"`
 	Tipo       string    `gorm:"type:varchar(20)"`
 	PersonaID  int64
@@ -37,7 +37,7 @@ type Cita struct {
 }
 
 type Agenda struct {
-	Id        int64     `gorm:"primaryKey;autoIncrement:true"`
+	Id        int64     `gorm:"primaryKey;autoIncrement:false"`
 	Fecha     time.Time `gorm:"type:timestamp"`
 	Jornada   string    `gorm:"type:varchar(15)"`
 	Tipo      string    `gorm:"type:varchar(20)"`
@@ -46,7 +46,7 @@ type Agenda struct {
 }
 
 type Autorizacion struct {
-	Id                int64  `gorm:"primaryKey;autoIncrement:true"`
+	Id                int64  `gorm:"primaryKey;autoIncrement:false"`
 	Nombre            string `gorm:"type:varchar(20)"`
 	Tipo              string `gorm:"type:varchar(20)"`
 	Url               string `gorm:"type:varchar(200)"`
